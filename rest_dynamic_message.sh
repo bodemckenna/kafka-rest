b0605f9cf4de6b756416f1d658e4078d16b4ea00
Colm McKenna <bodemckenna@gmail.com>
2018-09-04 10:38:54 +0100

    updating files

--- rest_dynamic_message.sh
+++ rest_dynamic_message.sh
@@ -6,18 +6,14 @@ sleep 5
  
 # Get info about one topic
 echo "Get info about topic colm_test_rest"
-curl "http://cp-kafka.icon.seli.gic.ericsson.se/topics/colm_test_rest"
+curl "http://cp-kafka.icon.seli.gic.ericsson.se/topics/dynamic_rest"
 echo ""
 sleep 5
  
 #Creating messages
 echo "Creating 10000 messages"
 date +"%T.%N"
-for i in {0..10}
-do
-curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" --data '{"records":[{"value":{"name": "testUser'$i'"}}]}' "http://cp-kafka.icon.seli.gic.ericsson.se/topics/colm_test_rest"
-echo $i
-done
+curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" --data '{"records":[{"value":{"name": "testUser'$i'"}}]}' "http://cp-kafka.icon.seli.gic.ericsson.se/topics/dynamic_rest/?[0-10000]" -w "%{time_connect},%{time_total},%{speed_download},%{http_code},%{size_download},%{url_effective}\n"
 date +"%T.%N"
  
 #curl -X POST -H "Content-Type: application/vnd.kafka.json.v2+json" --data '{"records":[{"value":{"name": "testUser"}}]}' "http://cp-kafka.rontgen002.seli.gic.ericsson.se/topics/colm_test_rest"
@@ -30,11 +26,13 @@ echo ""
  
 #subscribe the consumer to a topic
 echo "subscribe the consumer to a topic"
-curl -X POST -H "Content-Type: application/vnd.kafka.v2+json" --data '{"topics":["colm_test_rest"]}' http://cp-kafka.icon.seli.gic.ericsson.se/consumers/my_json_consumer/instances/my_consumer_instance/subscription
+curl -X POST -H "Content-Type: application/vnd.kafka.v2+json" --data '{"topics":["dynamic_rest"]}' http://cp-kafka.icon.seli.gic.ericsson.se/consumers/my_json_consumer/instances/my_consumer_instance/subscription
  
 sleep 15
 #consumer some data from a topic
 echo "Consuming data from topic"
+echo ""
+curl -X GET -H "Accept: application/vnd.kafka.json.v2+json"   http://cp-kafka.icon.seli.gic.ericsson.se/consumers/my_json_consumer/instances/my_consumer_instance/records
 echo ""
 curl -X GET -H "Accept: application/vnd.kafka.json.v2+json"   http://cp-kafka.icon.seli.gic.ericsson.se/consumers/my_json_consumer/instances/my_consumer_instance/records
 echo ""
